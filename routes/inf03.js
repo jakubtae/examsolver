f0abd983073e042e01b167fd92e3799b68304542
jakubtae <jakub.tomczyk.2005@gmail.com>
2023-03-16 09:42:22 +0100

    V0

---
+++ routes/inf03.js
@@ -0,0 +1,71 @@
+const express = require("express");
+const router = express.Router();
+
+router.get("/", (req, res) => {
+  res.render("inf03/inf03.ejs");
+});
+
+router.post("/upload", (req, res) => {
+  var multiparty = require("multiparty");
+  var form = new multiparty.Form();
+  var fs = require("fs");
+  form.parse(req, function (err, fields, files) {
+    //! GETTING THE UPLOADED FILE [SESSION] AS AN ARRAY OF OBJECTS
+    var sesjaPlik = files.sesjaFile[0];
+
+    var fileString = fs.readFileSync(sesjaPlik.path).toString();
+
+    var sesja = JSON.parse(fileString);
+
+    //! GETTING THE ALL QUESTIONS FILE AS AN ARRAY OF OBJECTS
+    var fileString2 = fs.readFileSync("data.json").toString();
+
+    var fileObj = JSON.parse(fileString2);
+
+    var wszystkie = fileObj.gotowce;
+
+    //! Declaring check var so it has global scope
+    var check;
+    var odpowiedzi = [];
+    //! CHECKING PART
+    // * LOOP START
+    for (var i = 0; i < sesja.length; i++) {
+      var Tresc = sesja[i].tresc;
+      //  console.log(i); TESTING COMMAND
+      // * LOOP START
+      for (var j = 0; j < wszystkie.length; j++) {
+        // console.log(j); TESTING COMMAND
+        var CzyTresc = wszystkie[j].tresc;
+        var CzyOdpowiedz = wszystkie[j].odpowiedz;
+        //! SPRAWDZ CZY Tresc PASUJE DO CzyTresc w tablicy gotowe;
+        check = JSON.stringify(Tresc) === JSON.stringify(CzyTresc);
+
+        //! JESLI TAK
+
+        if (check === true) {
+          //! ZAKONCZ PETLE
+          //! WEZ ODPOWIEDZ
+          //! DOPISZ DO TABELII ODPOWIEDZI
+          j = wszystkie.length;
+          odpowiedzi.push(CzyOdpowiedz);
+        }
+      }
+      // * END OF SECOND FOR LOOP
+      //! JESLI NIE
+      if (check === false) {
+        //! WYLOSUJ LICZBE OD 1 do 4
+        //! DOPISZ DO TABELII ODPOWIEDZI
+        odpowiedzi.push(false);
+      }
+    }
+    // * END OF FIRST FOR LOOP
+    var id = sesjaPlik.path.slice(5, sesjaPlik.path.length);
+    console.log(id);
+    console.log(odpowiedzi); //!AN ARRAY OF ANSWERS
+    res.render();
+    //! RENDERING A PAGE WITH THE CODE WITH ARRAY
+  });
+});
+
+
+module.exports = router;
---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+
+
+app.set("view engine", "ejs");
+
+app.use(express.static("public")); //! static folder declaration
+app.use(express.json())
+app.use(bodyParser.urlencoded({ limit: "10mB", extended: false })); //! for FORMS USAGE
+const adminRouter = require("./routes/admin");
+const inf03Router = require("./routes/inf03");
+
+app.use("/admin", adminRouter);
+app.use("/inf03", inf03Router);
+app.listen(3000);
